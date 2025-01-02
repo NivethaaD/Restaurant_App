c2219ddd3a4d88aa5ea383e12028e0abef8088a6
NivethaaD <nivethaad3@gmail.com>
2025-01-02 15:45:51 +0530

    Created form for authentication

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-from .views import RestaurantListView , RestaurantDetailView , home
-
+from .views import RestaurantListView , RestaurantDetailView , home , RegisterView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     
     path('', home, name='home'),  
     path('restaurants/', RestaurantListView.as_view(), name='restaurant-list'),
     path('restaurant/<int:pk>/', RestaurantDetailView.as_view(), name='restaurant_detail'),
+    path('register/', RegisterView.as_view(template_name='registration/register.html'), name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
 
--- restaurant/views.py
+++ restaurant/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic import ListView , DetailView
 from .models import Restaurant
 from .filters import RestaurantFilter
+from django.contrib import messages
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 def home(request):
     return render(request,'restaurant/home.html')
@@ -53,6 +57,15 @@ class RestaurantDetailView(DetailView):
         return context
 
 
+class RegisterView(FormView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, 'Your account has been created! You can now log in.')
+        return super().form_valid(form)
 
 
 
